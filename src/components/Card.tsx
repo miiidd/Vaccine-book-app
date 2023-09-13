770fbf8a0ecef28d8f8deff8313f8f2d762896e2
miiidd <mild.pimnut@gmail.com>
2023-09-13 10:43:25 +0700

    'assignment05'

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,15 +1,16 @@
 import styles from './card.module.css'
 import Image from 'next/image'
+import InteractiveCard from './InteractiveCard'
 
 export default function Card( {hospitalName, imgSrc} : {hospitalName:string, imgSrc:string}) {
     return (
-        <div className='w-1/5 h-[300px] rounded-lg shadow-lg'>
+        <InteractiveCard>
             <div className='w-full h-[83%] relative rounded-t-lg '>
                 <Image src={imgSrc} alt='Hospital Picture' fill={true} className='object-cover rounded-t-lg'/>
             </div>
-        <div className='w-full h-[20%] p-[10px] bg-amber-200 font-normal'>
+            <div className='w-full h-[20%] p-[10px] font-normal'>
                 {hospitalName}
             </div>
-        </div>
+        </InteractiveCard>
     )
 }
